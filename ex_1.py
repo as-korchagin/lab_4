e75f7ff206cb642b1b18a1a0416a91537229d8e1
as-korchagin <as.korchagin@yandex.ru>
2017-09-24 19:53:04 +0300

    ex_1 done

--- ex_1.py
+++ ex_1.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
-from librip.gens import field
+from lab_4.librip.gens import field
+from lab_4.librip.gens import gen_random
 
 goods = [
     {'title': 'Ковер', 'price': 2000, 'color': 'green'},
@@ -9,3 +9,8 @@ goods = [
 ]
 
 # Реализация задания 1
+
+
+get_gen_random = gen_random(1, 50, 9)
+get_field = field(goods, 'title', 'price')
+print(", ".join(map(str, get_gen_random)) + ' ' + ", ".join(map(str, get_field)))
--- librip/gens.py
+++ librip/gens.py
@@ -1,24 +1,17 @@
 import random
 
 
-# Генератор вычленения полей из массива словарей
-# Пример:
-# goods = [
-#    {'title': 'Ковер', 'price': 2000, 'color': 'green'},
-#    {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'}
-# ]
-# field(goods, 'title') должен выдавать 'Ковер', 'Диван для отдыха'
-# field(goods, 'title', 'price') должен выдавать {'title': 'Ковер', 'price': 2000}, {'title': 'Диван для отдыха', 'price': 5300}
-
 def field(items, *args):
     assert len(args) > 0
-    # Необходимо реализовать генератор 
+    result = list()
+    for item in items:
+        if len(args) == 1 and (item.get(args[0]) is not None):
+            result.append(item.get(args[0]))
+        elif len(args) > 1:
+            if None not in set(item.get(arg) for arg in args):
+                result.append({arg: item.get(arg) for arg in args})
+    return result
 
 
-# Генератор списка случайных чисел
-# Пример:
-# gen_random(1, 3, 5) должен выдать примерно 2, 2, 3, 2, 1
-# Hint: реализация занимает 2 строки
 def gen_random(begin, end, num_count):
-    pass
-    # Необходимо реализовать генератор
+    return list(random.randint(begin, end) for i in range(num_count))
