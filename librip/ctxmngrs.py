8d76fd031668842c095a68536a027873a81d9d3a
as-korchagin <as.korchagin@yandex.ru>
2017-09-25 15:13:45 +0300

    ex_5 done

--- librip/ctxmngrs.py
+++ librip/ctxmngrs.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 # Здесь необходимо реализовать
 # контекстный менеджер timer
 # Он не принимает аргументов, после выполнения блока он должен вывести время выполнения в секундах
@@ -6,3 +9,16 @@
 #   sleep(5.5)
 #
 # После завершения блока должно вывестись в консоль примерно 5.5
+
+class Timer:
+    time_in = None
+
+    def __init__(self):
+        self.time_in = datetime.datetime.now()
+
+    def __enter__(self):
+        return self.time_in
+
+    def __exit__(self, *args):
+        print((datetime.datetime.now() - self.time_in).total_seconds())
+        return True
