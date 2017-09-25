797529b779d905bf9c97850068b8fca9e049a731
as-korchagin <as.korchagin@yandex.ru>
2017-09-25 12:57:09 +0300

    ex_2 done

---
+++ ex_2.py
@@ -0,0 +1,8 @@
+from lab_4.librip.gens import gen_random
+from lab_4.librip.iterators import Unique
+
+data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
+data2 = gen_random(1, 3, 10)
+data3 = ['a', 'A', 'b', 'B']
+
+print(*filter(lambda i: i is not None, list(i for i in Unique(data3))), sep=", ")
