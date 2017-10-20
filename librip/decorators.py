be4c351e586b0801c4d94d7ba6ac9b94c3a18393
as-korchagin <as.korchagin@yandex.ru>
2017-10-20 22:53:05 +0300

    ex_4 fix

--- librip/decorators.py
+++ librip/decorators.py
@@ -3,26 +3,26 @@ def print_result(func):
         def decorate():
             try:
                 result = func(arg)
-            except TypeError:
-                result = func()
-            for i in result:
-                print(i)
-            print(func.__name__)
-            if arg is not None and type(result) is not tuple:
-                result = sorted(filter(lambda x: x is not None, list(i for i in result)),
-                              key=lambda x: x.lower())
-            if type(result) == list:
-                print(*result, sep="\n")
-            elif type(result) == dict:
-                for key in result.keys():
-                    print(key, result.get(key), sep=" = ")
-            elif type(result) == tuple:
-                print()
-            else:
-                print(result)
-            if arg is not None and type(result) is not tuple:
-                return result
+                for i in result:
+                    print(i)
+                print(func.__name__)
+                if arg is not None and type(result) is not tuple:
+                    result = sorted(filter(lambda x: x is not None, list(i for i in result)),
+                                    key=lambda x: x.lower())
+                if type(result) == list:
+                    print(*result, sep="\n")
+                elif type(result) == dict:
+                    for key in result.keys():
+                        print(key, result.get(key), sep=" = ")
+                elif type(result) == tuple:
+                    print()
+                else:
+                    print(result)
+                if arg is not None and type(result) is not tuple:
+                    return result
 
+            except TypeError:
+                print(func())
         return decorate()
 
     return a
